2140b5590bcaf4abfc15c5f6e8bfe6ff3baec176
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-01-21 02:10:51 +0530

    Routes Handled

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 //middlewares
 dotenv.config();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   connectDB();

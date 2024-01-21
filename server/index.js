06aff664c17a385fed1b2db6c17d9ddfb16c457e
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-01-21 18:35:59 +0530

    CommentRoutes Added

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const dotenv = require("dotenv");
 const cors = require("cors");
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
 //middlewares
 dotenv.config();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
 app.listen(PORT, () => {
   connectDB();
---
+++ server/routes/comments.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+
+//Create
+router.post("/create", async (req, res) => {
+  try {
+    const newComment = new Comment(req.body);
+    const saveComment = await newComment.save();
+    res.status(200).json(saveComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Update
+// implement when user wants to update its password user must provide his prev password
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(req.params.id,{ $set: req.body },{ new: true });
+    res.status(200).json(updatedComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.id);
+    res.status(200).json("Comment has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Get posts Comment 
+router.get("/post/:postId", async (req, res) => {
+  try {
+    const comments = await Comment.find({postId:req.params.postId});
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

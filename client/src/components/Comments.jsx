367794df40bb73fd439e5f7a02249afecfae1c6f
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-01-28 00:15:10 +0530

    CommentSec handled

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,22 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { MdDelete } from "react-icons/md";
 import { FiEdit } from "react-icons/fi";
+import { UserContext } from '../context/UserContext';
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { URL } from "../url"
 
-const Comments = () => {
+const Comments = ({ c, authorId, comments, setComments }) => {
+    const { user } = useContext(UserContext);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedComment, setEditedComment] = useState(c.comment);
+    const commentRef = useRef();
+    const handleDeleteComment = async (id) => {
+        try {
+            await axios.delete(URL + "/api/comments/" + id, { withCredentials: true });
+            setComments(comments.filter(comment => comment._id !== id));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const handleUpdateComment = async (id) => {
+        try {
+            await axios.put(URL + "/api/comments/" + id, { comment: editedComment }, { withCredentials: true });
+            setComments(comments.map(comment => (comment._id === id ? { ...comment, comment: editedComment } : comment)));
+            setIsEditing(false);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const handleEditComment = (c) => {
+        setIsEditing(true);
+        commentRef.current.focus();
+        const length = editedComment.length;
+        commentRef.current.setSelectionRange(length, length);
+    }
+
     return (
         <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
             <div className="flex items-center justify-between">
-                <h3 className="font-bold text-gray-600">@krishanu</h3>
-                <div className="flex justify-center items-center space-x-4">
-                    <p className="text-gray-500 text-sm">19/01/2024</p>
-                    <p className="text-gray-500 text-sm">16:45</p>
-                    <div className="flex items-center justify-center space-x-2">
-                        <p className="text-md sm:text-lg md:text-xl hover:scale-110 cursor-pointer"><FiEdit /></p>
-                        <p className="text-md sm:text-lg md:text-xl hover:scale-110 cursor-pointer"><MdDelete /></p>
+                <h3 className="font-bold text-gray-600">@{c.author}</h3>
+                <div className="flex justify-center items-center space-x-2">
+                    <p className="text-gray-500 text-sm">{new Date(c.updatedAt).toString().slice(4, 15)}</p>
+                    <p className="text-gray-500 text-sm">{new Date(c.updatedAt).toString().slice(16, 24)}</p>
+                    <div className="flex items-center justify-center pr-1 space-x-2" >
+                        {user?._id === c.userId && !isEditing && (
+                            <p onClick={() => handleEditComment(c)} className="text-lg md:text-xl hover:scale-110 cursor-pointer"><FiEdit /></p>
+                        )}
+                        {user?._id === c.userId && isEditing && (
+                            <>
+                                <p onClick={() => handleUpdateComment(c._id)} className="text-sm md:text-md border-2 border-gray-400 p-1 rounded-xl cursor-pointer hover:bg-gray-300 transition duration-300">Save</p>
+                                <p onClick={() => setIsEditing(false)} className="text-sm md:text-md border-2 border-gray-400 p-1 rounded-xl cursor-pointer hover:bg-gray-300 transition duration-300">Cancel</p>
+                            </>
+                        )}
+                        {
+                            (user?._id === authorId) | (user?._id === c.userId) ? <p onClick={() => handleDeleteComment(c._id)} className="text-lg md:text-xl hover:scale-110 cursor-pointer"><MdDelete /></p> : null
+                        }
                     </div>
                 </div>
             </div>
-            <p className="px-4 mt-2">Nice information!</p>
+            <input ref={commentRef} value={editedComment} onChange={(e) => setEditedComment(e.target.value)} type='text' className="px-4 mt-2 bg-gray-200 w-full outline-none overflow-auto"/>
         </div>
     )
 }
--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -13,6 +13,8 @@ import { UserContext } from "../context/UserContext";
 const PostDetails = () => {
     const postId = useParams().id;
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
     const { user } = useContext(UserContext);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
@@ -28,6 +30,19 @@ const PostDetails = () => {
             console.log(err);
         }
     }
+    useEffect(() => {
+        fetch();
+    }, [postId])
+
+    const fetchPostComment = async () => {
+        const res = await axios.get(URL + "/api/comments/post/" + postId);
+        setComments(res.data);
+    }
+
+    useEffect(() => {
+        fetchPostComment();
+    }, [comments]);
+
     const handlePostDeletion = async () => {
         try {
             const res = await axios.delete(URL + "/api/posts/" + postId, { withCredentials: true });
@@ -37,9 +52,15 @@ const PostDetails = () => {
             console.log(err);
         }
     }
-    useEffect(() => {
-        fetch();
-    }, [postId])
+    const handlePostComment = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(URL + "/api/comments/create", { comment, author: user.username, postId, userId: user._id }, { withCredentials: true });
+            setComment("");
+        } catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
@@ -50,7 +71,7 @@ const PostDetails = () => {
                         {
                             (user?._id == post.userId) ? (
                                 <div className="flex items-center justify-center space-x-2">
-                                    <p onClick={() => navigate(`/edit/${postId}`)}className="text-xl sm:text-2xl md:text-3xl hover:scale-110 cursor-pointer"><FiEdit /></p>
+                                    <p onClick={() => navigate(`/edit/${postId}`)} className="text-xl sm:text-2xl md:text-3xl hover:scale-110 cursor-pointer"><FiEdit /></p>
                                     <p onClick={handlePostDeletion} className="text-xl sm:text-2xl md:text-3xl hover:scale-110 cursor-pointer"><MdDelete /></p>
                                 </div>) : null
                         }
@@ -77,18 +98,22 @@ const PostDetails = () => {
                     </div>
                     <div className="flex flex-col mt-4">
                         <h3 className="mt-6 mb-4 font-semibold"></h3>
-                        <Comment />
+                        {
+                            comments?.map((c, i) => (
+                                <Comment key={i} c={c} authorId={post.userId} comments={comments} setComments={setComments}/>
+                            ))
+                        }
                     </div>
                     {/* write a comment */}
                     <div className="flex flex-col w-full mt-4 md:flex-row">
                         <input
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
                             type="text"
                             placeholder="Write a comment"
                             className="md:w-[80%] bg-slate-200 border-2 border-black outline-none px-4 mt-4 md:mt-0 rounded-lg h-14 md:rounded-l-xl md:rounded-r-none"
                         />
-                        <button
-                            className="bg-black text-white text-sm px-4 py-2 md:px-2 md:w-[20%] mt-4 md:mt-0 rounded-lg md:rounded-r-xl md:rounded-l-none"
-                        >
+                        <button onClick={handlePostComment} className="bg-black text-white text-sm px-4 py-2 md:px-2 md:w-[20%] mt-4 md:mt-0 rounded-lg md:rounded-r-xl md:rounded-l-none">
                             Add Comment
                         </button>
                     </div>

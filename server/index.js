c75cf177777bc3234151bf2d3c9d39cab54cec36
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-01-27 17:26:37 +0530

    Local Image Rendering

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 8080;
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const path = require("path");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
@@ -14,15 +16,37 @@ const commentRoute = require("./routes/comments");
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
+app.use("/images", express.static(path.join(__dirname, "images"))); 
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
+
+// image upload
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+    // console.log(req.body);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded successfully!");
+});
 
 app.listen(PORT, () => {
   connectDB();
